466dc137a6bd90172947ea3dff4a9fb8ec3c9bf4
ztlaistn <54722655+ztlaistn@users.noreply.github.com>
2022-03-24 15:57:37 -0400

    Initial commit
    Created basic template with Facebook integration

---
+++ functions.js
@@ -0,0 +1,13 @@
+function autoScroll(id) {
+    var element = document.getElementById(id);
+    element.scrollIntoView({behavior: "auto", block: "center", inline: "nearest"});
+}
+
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
