63f7a45ee7bde024320ef2b390af8a754057cd53
ztlaistn <ztlaistn@buffalo.edu>
2022-03-29 23:48:55 -0400

    Added multi-page navigation system
    -General information sections split into separate "pages" to reduce clutter
    -Hamburger menu now navigates to individual "pages"

--- functions.js
+++ functions.js
@@ -1,14 +1,35 @@
-function autoScroll(id) {
-    toggleMenu();
-    var element = document.getElementById(id);
-    element.scrollIntoView({behavior: "auto", block: "center", inline: "nearest"});
-}
-
 function toggleMenu() {
-  var menu = document.getElementById("menu");
+  let menu = document.getElementById("menu");
   if (menu.style.display === "block") {
     menu.style.display = "none";
   } else {
     menu.style.display = "block";
   }
+}
+
+function togglePage(id) {
+    toggleMenu();
+    let pages = ["home", "schedule", "artists-vendors", "about-us"]
+    for(let i=0; i<pages.length; i++){
+        if(pages[i] != id){
+            let page = document.getElementById(pages[i]);
+            page.style.display = "none";
+        } else {
+            let page = document.getElementById(id);
+            page.style.display = "block";
+        }
+    }
+}
+
+function firstLoad(id) {
+    let pages = ["home", "schedule", "artists-vendors", "about-us"]
+    for(let i=0; i<pages.length; i++){
+        if(pages[i] != id){
+            let page = document.getElementById(pages[i]);
+            page.style.display = "none";
+        } else {
+            let page = document.getElementById(id);
+            page.style.display = "block";
+        }
+    }
 }
